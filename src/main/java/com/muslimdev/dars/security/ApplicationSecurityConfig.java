b390d5d53890b91a6654f0919ceccc9ab59f91d0
@muslimdev0211 <zoirjonabdulloh@gmail.com>
2024-05-09 20:26:36 +0500

    First commit

--- src/main/java/com/muslimdev/dars/security/ApplicationSecurityConfig.java
+++ src/main/java/com/muslimdev/dars/security/ApplicationSecurityConfig.java
@@ -3,6 +3,7 @@ package com.muslimdev.dars.security;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.WebSecurityConfigurer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -39,6 +40,10 @@ public class ApplicationSecurityConfig {
                         .requestMatchers("/*", "/index", "/css", "/js/*")
                         .permitAll()
                         .requestMatchers("/api/**").hasRole(STUDENT.name())
+                        .requestMatchers(HttpMethod.DELETE, "/management/api/**").hasAuthority(ApplicationUsersPermissions.COURSES_WRITE.getPermission())
+                        .requestMatchers(HttpMethod.POST, "/management/api/**").hasAuthority(ApplicationUsersPermissions.COURSES_WRITE.getPermission())
+                        .requestMatchers(HttpMethod.PUT, "/management/api/**").hasAuthority(ApplicationUsersPermissions.COURSES_WRITE.getPermission())
+                        .requestMatchers(HttpMethod.GET,"/management/api/**").hasAnyRole(ADMIN.name(), ADMINTRAINEE.name())
                 .anyRequest()
                 .authenticated())
                 .httpBasic(Customizer.withDefaults());
@@ -52,17 +57,20 @@ public class ApplicationSecurityConfig {
         UserDetails annaSmithUser = User.builder()
                 .username("annasmith")
                 .password(passwordEncoder.encode("password"))
-                .roles(STUDENT.name())
+//                .roles(STUDENT.name())
+                .authorities(STUDENT.getGrantedAuthorities())
                 .build();
         UserDetails lindaUser = User.builder()
                 .username("linda")
                 .password(passwordEncoder.encode("password123"))
-                .roles(ADMIN.name())
+//                .roles(STUDENT.name())
+                .authorities(ADMIN.getGrantedAuthorities())
                 .build();
         UserDetails tomUser = User.builder()
                 .username("tom")
                 .password(passwordEncoder.encode("password123"))
-                .roles(ADMINTRAINEE.name())
+//                .roles(ADMINTRAINEE.name())
+                .authorities(ADMINTRAINEE.getGrantedAuthorities())
                 .build();
         return new InMemoryUserDetailsManager(
                 annaSmithUser,
--- src/main/java/com/muslimdev/dars/security/ApplicationUsersRole.java
+++ src/main/java/com/muslimdev/dars/security/ApplicationUsersRole.java
@@ -1,8 +1,11 @@
 package com.muslimdev.dars.security;
 
 import com.google.common.collect.Sets;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
 
 import java.util.Set;
+import java.util.stream.Collectors;
 
 public enum ApplicationUsersRole {
     STUDENT(Sets.newHashSet()),
@@ -21,6 +24,14 @@ public enum ApplicationUsersRole {
     }
 
     public Set<ApplicationUsersPermissions> getPermissions() {
+        return permissions;
+    }
+    public Set<SimpleGrantedAuthority> getGrantedAuthorities(){
+        Set<SimpleGrantedAuthority> permissions = getPermissions().stream()
+                .map(permission -> new SimpleGrantedAuthority(permission.getPermission()))
+                .collect(Collectors.toSet());
+        permissions.add(new SimpleGrantedAuthority("ROLE_" +  this.name()));
+
         return permissions;
     }
 }
